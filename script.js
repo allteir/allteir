8f46bc4fe65a145e0faf2e8fbc2f2c86d0f047c9
kuro <arsware9@gmail.com>
2024-12-02 05:47:31 +0700

    tes

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ var toolproject = document.getElementById("toolproject");
 
 document.addEventListener("click",(e)=>{
     if(e.target == toolproject||e.target==toolproject.children[0]||e.target==toolproject.children[1]){
-        window.location = "/projects/toolproject.html"
+        window.location = "./projects/toolproject.html"
     }
 })
